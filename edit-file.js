46f924fb31b649a41d2a16adebcd806f38791672
Jonathon Schwamman <schwamman@msn.com>
2019-09-18 17:19:56 -0500

    Reading and writing completed with callbacks

--- edit-file.js
+++ edit-file.js
@@ -2,5 +2,31 @@
 
 const fs = require('fs');
 
-const fileRead = `${__dirname}/files/data/person.json`;
-console.log(fileRead);
+const file = process.argv[2];
+const readData = [];
+
+//Reads and modifies data in file passed in from console
+
+fs.readFile(file, (err, data) => {
+  if (err) throw err;
+  readData.push(JSON.parse(data));
+
+  console.log(readData);
+
+  modifyData();
+  fs.writeFile(file, JSON.stringify(readData), (err) => {
+    if (err) throw err;
+    console.log('Written to file!');
+  });
+})
+
+function modifyData() {
+  readData[0].firstName = 'Jack';
+  readData[0].lastName = 'Sparrow';
+  readData[0].hair.type = 'long';
+  readData[0].favoriteFoods = 'Rum';
+  console.log(readData);
+}
+
+
+
---
+++ edit-file.test.js
@@ -0,0 +1,17 @@
+'use strict';
+const fs = require('fs');
+
+describe('fs.readFile', () => {
+  it('adds read data to array readData', done => {
+    const file = `${__dirname}/files/data/person.json`;
+    const readData = [];
+
+    fs.readFile(file, (err, data) => {
+      if (err) throw err;
+      readData.push(JSON.parse(data));
+      expect(readData.length).toEqual(1);
+      done();
+    });
+
+  });
+});
